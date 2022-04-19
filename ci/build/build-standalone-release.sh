23d6bc1e0208de6ad3b7d817a3b31634c9bae350
Edouard VINCENT <edvincent@users.noreply.github.com>
2022-04-19 03:44:06 +0000

    fix: Generate shrinkwraps for the bundled vscode

--- ci/build/build-standalone-release.sh
+++ ci/build/build-standalone-release.sh
@@ -29,6 +29,46 @@ main() {
 
   cd "$RELEASE_PATH"
   yarn --production --frozen-lockfile
+
+  create_shrinkwraps
+}
+
+create_production_shrinkwrap() {
+  npm shrinkwrap
+
+  # HACK@edvincent: The shrinkwrap file will contain the devDependencies, which by default
+  # are installed if present in a lockfile. To avoid every user having to specify --production
+  # to skip them, we carefully remove them from the shrinkwrap file.
+  json -f npm-shrinkwrap.json -I -e "Object.keys(this.dependencies).forEach(dependency => { if (this.dependencies[dependency].dev) { delete this.dependencies[dependency] } } )"
+
+  # HACK@edvincent: We create the shrinkwrap file from the installed node_modules folder.
+  # Installing node-addon-api also creates an auto-generated folder under @parcel/node-addon-api for gyp,
+  # but this actually does not have a package.json (nor it's a package that can be fetched from the repository).
+  # Thus `npm shrinkwrap` doesn't know how to generate a lock entry for it, and leaves it empty - which then
+  # breaks any subsequent install. We manually remove it, as on every install it will be auto-generated.
+  json -f npm-shrinkwrap.json -I -e "if (this.dependencies['@parcel/node-addon-api'] == {}) { delete this.dependencies['@parcel/node-addon-api'] }"
+}
+
+create_shrinkwraps() {
+  # yarn.lock or package-lock.json files (used to ensure deterministic versions of dependencies) are
+  # not packaged when publishing to the NPM registry.
+  # To ensure deterministic dependency versions (even when code-server is installed with NPM), we create
+  # an npm-shrinkwrap.json file from the currently installed node_modules. This ensures the versions used
+  # from development (that the yarn.lock guarantees) are also the ones installed by end-users
+
+  # We first generate the shrinkwrap file for code-server itself - from being in $RELEASE_PATH
+  create_production_shrinkwrap
+
+  # Then the shrinkwrap files for the bundled VSCode
+  # We don't need to remove the devDependencies for these because we control how it's installed - and
+  # as such we can force the --production flag
+  cd lib/vscode/
+  create_production_shrinkwrap
+
+  cd extensions/
+  create_production_shrinkwrap
+
+  cd ../../
 }
 
 main "$@"
